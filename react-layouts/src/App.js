d7e3117ea6ab5abd57add278763a231f2da6e3c7
Alexander Knyazev <AlexandrKnyazev1395@mail.ru>
2018-01-17 23:05:27 +0300

    scroll state for "App" component

--- react-layouts/src/App.js
+++ react-layouts/src/App.js
@@ -4,17 +4,48 @@ import {
   Route,
 } from 'react-router-dom'
 
-
 import Header from './components/Header'
 import MainPage from './components/MainPage'
 import { CreateEvent, EditEvent } from './components/Event'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+       scrollTopPixels: 0,
+       scrollLeftPixels: 0
+    }
+  }
+
+  componentDidMount = () => {
+    const scrollLeft = window.pageXOffset;
+    const scrollTop = window.pageYOffset;
+    this.setState({
+      scrollLeftPixels: scrollLeft,
+      scrollTopPixels: scrollTop
+    })
+    window.addEventListener('scroll', this.handleScroll); 
+  }
+
+  handleScroll = () => {
+    const scrollLeft = window.pageXOffset;
+    const scrollTop = window.pageYOffset;
+    this.setState({
+      scrollLeftPixels: scrollLeft,
+      scrollTopPixels: scrollTop
+    })
+  }
+  
+
   render() {
+    const scrollPixels = {
+      top: scrollTopPixels,
+      left: scrollLeftPixels
+    }
     return (
       <Router>
         <div className="App">
-          <Header />
+          <Header scrollPixels={scrollPixels}/>
           <Route exact path="/" component={MainPage} />
           <Route exact path="/editEvent" component={EditEvent} />
           <Route exact path="/createEvent" component={CreateEvent} />
--- react-layouts/src/components/Event/Create/Footer.js
+++ react-layouts/src/components/Event/Create/Footer.js
@@ -13,7 +13,6 @@ export default class Footer extends Component {
   }
 
   handleSaveButtonClick = () => {
-    debugger;
     this.setState({
       isEventSaved: true
     })
